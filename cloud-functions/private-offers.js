242e48fedb18d3d9d60b1e6f3153bddb278344d4
John Aronson <john.aronson@appdynamics.com>
2020-11-20 18:05:10 -0700

    password checking

--- cloud-functions/private-offers.js
+++ cloud-functions/private-offers.js
@@ -1,8 +1,30 @@
-exports.handler = function(event, context, callback) 
+exports.handler = function(event, context, callback)
     {
-        callback(null, 
-            {
-                statusCode: 200,
-                body: "These are you private travel offers for today:"
-            } )
+        const content = `
+                <h3>Welcome To Your Private Travel Offers for Today!</h3>
+                <ul>Trip to your <strong>backyard</strong></ul>
+                <ul>Trip to around the <strong>block</strong></ul>
+            `
+        let body;
+        
+        if(event.body)
+            body = JSON.parse(event.body);    
+        else
+            body = { };
+
+        if(body.password == "WebDeveloper")
+        {
+            callback(null, 
+                {
+                    statusCode: 200,
+                    body: content
+                } )
+        } else               
+        {
+            callback(null, 
+                {
+                    statusCode: 401,
+                    body: "Incorrect Password"
+                } )
+        }                
     };
